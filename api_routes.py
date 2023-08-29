95087a6022a3249a14bd8b737f676788997cece8
Sarthak Gautam <96220233+iSarthakGautam@users.noreply.github.com>
2023-08-29 18:02:10 +0530

    Update api_routes.py

--- api_routes.py
+++ api_routes.py
@@ -414,7 +414,7 @@ class Test_module(Resource):
                   time_limit_seconds=args["time_limit_seconds"])
 
     db.session.add(a)
-    b = Notification(notification_title="New Test:"args["assesment_name"],
+    b = Notification(notification_title="New Test:"+args["assesment_name"],
                      notification_message=args["assesment_type"])
     db.session.add(b)
     db.session.commit()
